3e20f9aec9a3f25df50e488ed52baee8c5985865
LangBraian <braianelang@gmail.com>
2023-07-18 12:27:18 -0300

    Parte_Dos_Desarrollo

--- blog/apps/posts/admin.py
+++ blog/apps/posts/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Categoria, Post
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'subtitulo', 'fecha', 'texto', 'activo', 'categoria', 'imagen', 'publicado')
+
+
+
+
+admin.site.register(Categoria)
